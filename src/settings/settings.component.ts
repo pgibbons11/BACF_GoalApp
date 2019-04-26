81863eedfbc1c90292b829c098e1c07c4e357429
Patrick Gibbons <pgibbons11@gmail.com>
2019-04-26 18:16:55 -0400

    added settings page, started on dashboard

--- src/settings/settings.component.ts
+++ src/settings/settings.component.ts
@@ -1,23 +1,35 @@
 import { Component, OnInit } from "@angular/core";
+import { RouterExtensions } from "nativescript-angular/router";
 
 /* ***********************************************************
 * Before you can navigate to this page from your app, you need to reference this page's module in the
 * global app router module. Add the following object to the global array of routes:
-* { path: "profile", loadChildren: "./profile/profile.module#ProfileModule" }
+* { path: "settings", loadChildren: "./settings/settings.module#SettingsModule" }
 * Note that this simply points the path to the page module file. If you move the page, you need to update the route too.
 *************************************************************/
 
+class Setting {
+  constructor(public name: String) { }
+}
+
+let names = ["Setting 1", "Setting 2", "Setting 3", "Setting 4", "Setting 5", "Setting 6", "Setting 7"]
+
+
 @Component({
-    selector: "Profile",
+    selector: "Settings",
     moduleId: module.id,
-    templateUrl: "./profile.component.html",
-    styleUrls: ["./profile.component.css"]
+    templateUrl: "./settings.component.html",
+    styleUrls: ["./settings.component.css"]
 })
-export class ProfileComponent implements OnInit {
-    constructor() {
-        /* ***********************************************************
-        * Use the constructor to inject app services that you need in this component.
-        *************************************************************/
+export class SettingsComponent implements OnInit {
+    public settings: Array<Setting>;
+
+    constructor(private routerExtensions: RouterExtensions) {
+        this.settings = [];
+
+        for(let i = 0; i < names.length; i++) {
+          this.settings.push(new Setting(names[i]));
+        }
     }
 
     ngOnInit(): void {
@@ -25,4 +37,8 @@ export class ProfileComponent implements OnInit {
         * Use the "ngOnInit" handler to initialize data for this component.
         *************************************************************/
     }
+
+    goBack(): void {
+		    this.routerExtensions.back();
+	  }
 }
